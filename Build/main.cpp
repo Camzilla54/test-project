ec526dad59490984fc34d558077b42e7eeccc6ad
Cameron Houston <cameronhouston@Camerons-MacBook-Air.local>
2025-09-12 17:26:18 -0600

    first commit

---
+++ Build/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main()
+{
+    std::cout << "CMake + Ninja + VS Code are working!\n";
+    return 0;
+}
---
+++ main.cpp
@@ -0,0 +1,2 @@
+#include <iostream>
+int main() { std::cout << "CMake+Ninja OK\n"; }
